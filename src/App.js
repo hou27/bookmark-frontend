2202dd0cd7536ed60d9bc6928a80a58bd3b51723
hou27 <ataj125@gmail.com>
2022-08-25 20:34:26 +0900

    feat: flarelane 기본 설정

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ import GlobalStyles from "./theme/globalStyles";
 // components
 import ScrollToTop from "./components/ScrollToTop";
 import { BaseOptionChartStyle } from "./components/charts/BaseOptionChart";
+import FlareLane from "@flarelane/flarelane-web-sdk";
 
 // ----------------------------------------------------------------------
+FlareLane.initialize({
+  projectId: "d9873220-bd4d-41c2-afeb-d658575e920d",
+  serviceWorkerPath: "/sw.js",
+});
 
 export default function App() {
   return (
